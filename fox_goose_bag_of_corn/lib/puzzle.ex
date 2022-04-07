bcadb8163320f6ab5dceeae9798a8b4e97e82ee6
ReginaAyala <regis3096@gmail.com>
2022-04-06 20:29:02 -0500

    Kata 4 terminada

--- fox_goose_bag_of_corn/lib/puzzle.ex
+++ fox_goose_bag_of_corn/lib/puzzle.ex
@@ -3,34 +3,83 @@ defmodule FoxGooseBagOfCorn.Puzzle do
   @start_position [
     [[:fox, :goose, :corn, :you], [:boat], []]
   ]
+  @bad_position [
+    [:fox, :goose],
+    [:goose, :fox],
+    [:corn, :goose],
+    [:goose, :corn],
+    [:fox, :goose, :corn],
+    [:fox, :corn, :goose],
+    [:goose, :fox, :corn],
+    [:goose, :corn, :fox],
+    [:corn, :fox, :goose],
+    [:corn, :goose, :fox]
+  ]
 
-  def river_crossing_plan do
-     # Implement this with an algorithm.
-     # Hard coding the solution is easy! ;-)
-    [
-      [[:fox, :goose], [:boat, :you, :corn, :goose], [:you]],
-      [[:goose, :corn], [:boat], [:you]]
-    ] 
+  def move([a, b, c], from: :left, to: :center) do
+    Enum.map(a, fn x -> 
+    [a -- [:you, x], b ++ [:you, x], c]
+    end)
+    |> Enum.map(fn x -> Enum.map(x, fn e -> Enum.uniq(e) end) end)
+  end 
+
+  def move([a, b, c], from: :center, to: :right) do 
+    Enum.map(b -- [:boat], fn x ->
+      [a, b -- [:you, x], c ++ [:you, x]]
+    end)
+    |> Enum.map(fn x -> Enum.map(x, fn e -> Enum.uniq(e) end) end)
   end
 
-  def to_set(list) when is_list(list), do: Enum.into(list, HashSet.new)
-  def step_to_sets(step), do: step |> Enum.map &to_set/1
+  def move([a, b, c], from: :right, to: :center) do
+    Enum.map(c, fn x -> 
+      [a, b ++ [:you, x], c -- [:you, x]]
+    end)
+    |> Enum.map(fn x -> Enum.map(x, fn e -> Enum.uniq(e) end) end)
+  end
 
-  @doc """
-    Defines a sigil for a HashSet of atoms
+  def move([a, b, c], from: :center, to: :left) do
+    Enum.map(b -- [:boat], fn x ->
+      [a ++ [:you, x], b -- [:you, x], c]
+    end)
+    |> Enum.map(fn x -> Enum.map(x, fn e -> Enum.uniq(e) end) end)
+  end
+  
+  def exceptions([a, b, c]) do
+    not Enum.member?(@bad_position, a) and
+    not Enum.member?(@bad_position, b) and
+    not Enum.member?(@bad_position, c) and 
+    (length(b) <= 3)
+  end 
 
-    Example: 
-    iex> import #{__MODULE__}
-    iex> ~H":a :b :c"
-    #HashSet<[:c, :b, :a]>
-    iex> ~H""
-    #HashSet<[]>
-  """
-  def sigil_H(str, _opts) do
-    str |> String.split(" ") 
-        |> Enum.filter(&String.length(&1) > 0)
-        |> Enum.map(&String.replace(&1,":","")) 
-        |> Enum.map(&String.to_atom/1)
-        |> Enum.into(HashSet.new)
+  def next_position(from: l, to: r) do
+    case {l, r} do
+      {:left, :center} -> {:center, :right} 
+      {:center, :right} -> {:right, :center}
+      {:right, :center} -> {:center, :left}
+      {:center, :left} -> {:left, :center}
+    end
+  end
+
+  def next_move([[a, b, c] | ls] = plan, from: l, to: r) do
+    x =
+      move([a, b, c], from: l, to: r)
+      |> IO.inspect()
+      |> Enum.filter(&exceptions/1)
+      |> Enum.filter(fn z -> z not in plan end)
+
+    if length(x) != 0 do
+      x |> Enum.flat_map(fn f -> 
+            {n_from, n_to} = next_position(from: l, to: r)
+            next_move([f | plan], from: n_from, to: n_to)
+           end)
+    else 
+      [plan]
+    end
+  end
+
+  def river_crossing_plan do
+    next_move(@start_position, from: :left, to: :center)
+    |> List.last()
+    |> Enum.reverse()
   end
 end
